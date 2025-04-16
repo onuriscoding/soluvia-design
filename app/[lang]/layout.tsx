0cecd5bddca2beb313b5aa397fa6a52f9de24112
onuriscoding <onurdogancs@gmail.com>
2025-04-16 15:16:28 +0200

    google search icon not appearing fix

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -37,8 +37,11 @@ export default async function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="google-site-verification" content="C6PFuzOdUFnB3OIMk6MlfM6u0Q7326NrvzVxrj58Ilo" />
-        <link rel="icon" href="../favicon.ico" />
-        <link rel="apple-touch-icon" href="../favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
         
         {/* Logo Specific Structured Data */}
